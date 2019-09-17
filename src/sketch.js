7b6ec7544bc6c1fda0fb40a6175b018105596f7c
Pranesh Kamalakanthan <prkama@mlc353.na.sas.com>
2019-09-17 15:50:00 -0400

    bpm working. adding sound

--- src/sketch.js
+++ src/sketch.js
@@ -1,41 +1,80 @@
+const bass = new p5.Oscillator();
+
 let s = (p5) => {
     let layers = [];
     
     const dim = (x, y) => [window.innerWidth * x, window.innerHeight * y];
 
-    const DIM = [960, 640];
+    const D = [1050, 640];
 
     const SAMPLES = 16;
+
+    const FPS = 120;
+
+    let BPM = 80;
+
+    let step = 0;
 
     const center = (dim) => [ 
         window.innerWidth / 2 - dim[0] / 2, 
         window.innerHeight / 2 - dim[1] / 2,
         dim[0], dim[1]
     ];
+    
+    let gutter = D[0]/SAMPLES/4 * 1.5;
+
+    let s = [
+        (D[0] - gutter * SAMPLES*1.1) / SAMPLES,
+        D[1] * .15
+    ];
+
+    const sample = (on, sample, context) => {
+        p5.noStroke();
+        on ? p5.fill(199,185,110) : p5.fill(149,134,58);
+        p5.rect(context.x, context.y, context.w, context.h);
+        if(on) {
+            sample.amp(.2);
+            sample.freq(context.x);
+        } else {
+        }
+    }
 
     const canvas = () => {
         p5.background(255);
-        p5.fill(20, 30, 40);
-        p5.rect(0, 0, DIM[0], DIM[1]);
+        p5.fill(40, 40, 50);
+        p5.rect(0, 0, D[0], D[1]);
         p5.stroke(255);
-        p5.rect(0, DIM[1]/3.5, DIM[0], DIM[1] / 2.5);
-        p5.stroke(255);
+        p5.line(0, D[1]* .2, D[0], D[1]* .2);
+        p5.line(0, D[1]* .7, D[0], D[1]* .7);
+
+        if(Math.floor(p5.frameCount % (FPS*60/BPM/(SAMPLES))) === 0) {
+            step = (step + 1) % SAMPLES;        
+        }
         for(let i = 0; i < SAMPLES; i++) {
-            p5.fill(200);
-            p5.rect(i  * (DIM[0]/SAMPLES) + 5, DIM[1] - 160, 40, 100);
+            let context = {
+                x: i * (s[0] + gutter) + gutter*1.25,
+                y: D[1] - s[1] * 1.5,
+                w: s[0],
+                h: s[1]
+            };
+            sample(i===step, bass, context);         
         }
     }
 
     // sk.translate(window.innerWidth/2,window.innerHeight/2);
     p5.setup = () => {
+        p5.frameRate(FPS);
         p5.createCanvas(window.innerWidth,window.innerHeight);
+        bass.setType('sine');
+        bass.amp(0.5);
+        bass.start();
  
     }
 
     p5.draw = () => {
         canvas();
-
     }
 
 }
+
 const P5 = new p5(s);
