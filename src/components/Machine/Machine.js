fa0171d8b9126bf46a83fcb5d649732882707891
Pranesh Kamalakanthan <prkama@mlc353.na.sas.com>
2020-01-21 16:29:31 -0500

    using refs for animation frame

--- src/components/Machine/Machine.js
+++ src/components/Machine/Machine.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import Sample from '../Sample/Sample';
 import './Machine.scss';
 
@@ -6,18 +6,20 @@ export default function Machine() {
   const FPS = 60;
   const SAMPLES = 16;
   const VOICES = SAMPLES;
+  const [bpm, setBpm] = useState(120);
 
   const audioContext = new (window.AudioContext || window.webkitAudioContext)();
 
   const voices = Array(VOICES).fill(audioContext.createOscillator());
 
   // Creates list of sample components
-  function renderSamples() {
+  function renderSamples(on) {
     const ret = [];
     for (let i = 0; i < SAMPLES; i++) {
       ret.push(Sample({
         id: i,
         osc: voices[i % VOICES],
+        on: on === i,
       }));
     }
     return ret;
@@ -25,17 +27,35 @@ export default function Machine() {
 
   const [samples, setSamples] = useState(renderSamples());
 
-
   // setSamples(renderSamples(0));
+  let start = null;
+  const last = useRef(0);
+  const requestRef = useRef();
+  const [beat, setBeat] = useState(0);
+  // Run for ten seconds
+  function step(now) {
+    if (!start) start = now;
+
+    if (now - last >= 1000 / (bpm / 60)) {
+      last.current = now;
+      setBeat((beat + 1) % SAMPLES);
+      // setSamples(renderSamples(beat));
+      console.log('thump');
+    }
+    requestRef.current = window.requestAnimationFrame(step);
+  }
 
 
   useEffect(() => {
-  });
+    requestRef.current = window.requestAnimationFrame(step);
+    return () => window.cancelAnimationFrame(requestRef.current);
+  }, []);
 
   return (
     <div className="Machine">
       <div className="samples">
         {samples}
+        <div>{beat}</div>
       </div>
     </div>
   );
--- src/components/Sample/Sample.js
+++ src/components/Sample/Sample.js
@@ -2,11 +2,11 @@ import React, { useState } from 'react';
 import './Sample.scss';
 
 export default function Sample({
-  osc, on, selected, id,
+  osc, on, selected = false, id,
 }) {
   const labelId = `sample-${id}`;
   return (
-    <div className="Sample">
+    <div className={`Sample ${selected ? 'on' : ''}`}>
       {/* <input
         type="radio"
         name="sample"
